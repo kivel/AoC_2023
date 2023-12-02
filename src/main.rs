9dbe5b5aa4956d25e77819144df5f91f56ba7feb
Niko Kivel <niko.kivel@psi.ch>
2023-12-01 20:53:08 -0600

    day1 solutions

---
+++ src/main.rs
@@ -0,0 +1,109 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::path::Path;
+
+struct AoC;
+
+impl AoC {
+    // Returns an Iterator to the Reader of the lines of the file.
+    pub fn read_lines<P>(filename: P) -> std::io::Result<std::io::Lines<std::io::BufReader<File>>>
+    where
+        P: AsRef<Path>,
+    {
+        let file = File::open(filename)?;
+        Ok(std::io::BufReader::new(file).lines())
+    }
+
+    pub fn read_file(filename: &str) -> std::io::Result<Vec<String>> {
+        let org_data = match AoC::read_lines(filename) {
+            Ok(lines) => lines.collect::<Result<_, _>>().unwrap(),
+            Err(e) => panic!("Error opening file {}: {}", filename, e),
+        };
+        Ok(org_data)
+    }
+}
+
+fn day1_1(data: &Vec<String>) -> u32 {
+    data.iter()
+        .map(|l| {
+            let characters = l.chars();
+            let digits = &characters
+                .filter(|c| c.is_digit(10))
+                .map(|c| c.to_digit(10).unwrap() as u32)
+                .collect::<Vec<u32>>();
+            digits[0] * 10 + digits[digits.len() - 1]
+        })
+        .collect::<Vec<u32>>()
+        .iter()
+        .sum::<u32>()
+}
+
+fn day1_2(data: &Vec<String>) -> u32 {
+    let data = replace_number_literals_s(data);
+    data.iter()
+        .map(|l| {
+            let characters = l.chars();
+            let digits = &characters
+                .filter(|c| c.is_digit(10))
+                .map(|c| c.to_digit(10).unwrap() as u32)
+                .collect::<Vec<u32>>();
+            digits[0] * 10 + digits[digits.len() - 1]
+        })
+        .collect::<Vec<u32>>()
+        .iter()
+        .sum::<u32>()
+}
+
+fn replace_number_literals_s(data: &Vec<String>) -> Vec<String> {
+    data.iter()
+        .map(|l| {
+            let mut l = l.to_string();
+            l = l.replace("one", "o1e");
+            l = l.replace("two", "t2o");
+            l = l.replace("three", "t3e");
+            l = l.replace("four", "4");
+            l = l.replace("five", "5e");
+            l = l.replace("six", "6");
+            l = l.replace("seven", "7n");
+            l = l.replace("eight", "e8t");
+            l = l.replace("nine", "n9e");
+            l
+        })
+        .collect::<Vec<String>>()
+}
+
+fn main() {
+
+
+    let d = AoC::read_file("./input/day1_1_test.txt").unwrap();
+    let sum = day1_1(&d);
+    println!("result: {sum}");
+
+    let d = AoC::read_file("./input/day1_2.txt").unwrap();
+    let sum = day1_2(&d);
+    println!("result: {sum}");
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day1_2;
+
+    #[test]
+    fn d2() {
+        let result = day1_2(&vec!["two1nine".to_string()]);
+        assert_eq!(result, 29);
+        let result = day1_2(&vec!["eightwothree".to_string()]);
+        assert_eq!(result, 83);
+        let result = day1_2(&vec!["abcone2threexyz".to_string()]);
+        assert_eq!(result, 13);
+        let result = day1_2(&vec!["xtwone3four".to_string()]);
+        assert_eq!(result, 24);
+        let result = day1_2(&vec!["4nineeightseven2".to_string()]);
+        assert_eq!(result, 42);
+        let result = day1_2(&vec!["zoneight234".to_string()]);
+        assert_eq!(result, 14);
+        let result = day1_2(&vec!["7pqrstsixteen".to_string()]);
+        assert_eq!(result, 76);
+    }
+
+}
