b4c45153042ae5aabb5dec2cfa923b7956e4d869
Niko Kivel <niko.kivel@psi.ch>
2023-12-10 22:44:39 -0600

    day 10 part 10, moves

--- src/bin/day10_1.rs
+++ src/bin/day10_1.rs
@@ -1,5 +1,6 @@
 #[path = "../advent_of_code/mod.rs"]
 mod advent_of_code;
+use std::ops::{Add, Sub};
 
 fn day10_1(data: &Vec<String>) -> i32 {
     todo!()
@@ -10,6 +11,27 @@ struct Point {
     row: usize,
     col: usize,
 }
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Point {
+            row: self.row + other.row,
+            col: self.col + other.col,
+        }
+    }
+}
+
+impl Sub for Point {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Point {
+            row: self.row - other.row,
+            col: self.col - other.col,
+        }
+    }
+}
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 struct Tile {
     item: char,
@@ -18,18 +40,43 @@ struct Tile {
 }
 
 impl Tile {
-    fn get_start_connections(&self, grid: Vec<Vec<char>>, start_tile: &Tile) -> Vec<Tile> {
-        // let d = advent_of_code::Reader::read_file("./input/day10_loop.txt").unwrap();
-        // let grid = d
-        //     .iter()
-        //     .map(|line| line.chars().collect::<Vec<char>>())
-        //     .collect::<Vec<Vec<char>>>();
+    fn from_points(origin: Point, position: Point, grid: &Vec<Vec<char>>) -> Tile {
+        Tile {
+            item: grid[position.row][position.col],
+            position,
+            origin,
+        }
+    }
+    fn find_start_tile(grid: &Vec<Vec<char>>) -> Tile {
+        let start_row = grid
+            .iter()
+            .enumerate()
+            .filter_map(|(i, row)| if row.contains(&'S') { Some(i) } else { None })
+            .nth(0)
+            .unwrap();
 
-        // let start_tile = Tile {
-        //     item: 'S',
-        //     position: Point { row: 1, col: 1 },
-        //     origin: Point { row: 0, col: 0 },
-        // };
+        let row = grid[start_row].clone();
+        let start_col = row
+            .iter()
+            .enumerate()
+            .filter_map(|(j, &c)| if c == 'S' { Some(j) } else { None })
+            .nth(0)
+            .unwrap();
+
+        Tile {
+            item: 'S',
+            position: Point {
+                row: start_row,
+                col: start_col,
+            },
+            origin: Point {
+                row: start_row,
+                col: start_row,
+            },
+        }
+    }
+
+    fn get_start_connections(&self, grid: &Vec<Vec<char>>, start_tile: &Tile) -> Vec<Tile> {
         let mut connections: Vec<Tile> = Vec::<Tile>::new();
         // valid connections:
         //    |
@@ -108,9 +155,57 @@ impl Tile {
             }),
             _ => (),
         }
-        assert_eq!(connections.len(), 2);
-        println!("{:?}", &connections);
         connections
+    }
+
+    fn get_next_tile(&self, grid: &Vec<Vec<char>>) -> Option<Tile> {
+        let mut p = self.position.clone();
+        match self.item {
+            '|' => {
+                if self.origin.row < self.position.row {
+                    p.row += 1;
+                } else {
+                    p.row -= 1;
+                }
+                return Some(Tile::from_points(self.position, p, grid));
+            }
+            'L' => {
+                if self.origin.row < self.position.row {
+                    p.col += 1;
+                }
+                if self.origin.col > self.position.col {
+                    p.row -= 1;
+                }
+                return Some(Tile::from_points(self.position, p, grid));
+            }
+            '-' => {
+                if self.origin.col < self.position.col {
+                    p.col += 1;
+                } else {
+                    p.col -= 1;
+                }
+                return Some(Tile::from_points(self.position, p, grid));
+            }
+            'J' => {
+                if self.origin.row < self.position.row {
+                    p.col -= 1;
+                }
+                if self.origin.col < self.position.col {
+                    p.row -= 1;
+                }
+                return Some(Tile::from_points(self.position, p, grid));
+            }
+            '7' => {
+                if self.origin.row > self.position.row {
+                    p.col -= 1;
+                }
+                if self.origin.col < self.position.col {
+                    p.row += 1;
+                }
+                return Some(Tile::from_points(self.position, p, grid));
+            }
+            _ => None,
+        }
     }
 }
 fn main() {
@@ -160,10 +255,9 @@ mod tests {
             },
         };
 
-        println!("grid: {:?}", &grid);
-        println!("start: {:?}", &start_tile);
-        let connections = start_tile.get_start_connections(grid, &start_tile);
-        println!("connections: {:?}", &connections);
+        println!("{start_tile:?}");
+        assert_eq!(start_tile.position.row, 1);
+        assert_eq!(start_tile.position.col, 1);
     }
 
     #[test]
@@ -259,6 +353,39 @@ mod tests {
         }
         assert_eq!(connections.len(), 2);
         println!("{:?}", &connections);
+    }
+
+    #[test]
+    fn walk_test() {
+        let d = advent_of_code::Reader::read_file("./input/day10_loop.txt").unwrap();
+        let grid = d
+            .iter()
+            .map(|line| line.chars().collect::<Vec<char>>())
+            .collect::<Vec<Vec<char>>>();
+
+        let start_tile = Tile::find_start_tile(&grid);
+        let connections = start_tile.get_start_connections(&grid, &start_tile);
+
+        let mut path: Vec<Tile> = vec![start_tile, connections[1]];
+
+        let next_tile = &path.iter().last().unwrap().get_next_tile(&grid).unwrap();
+        path.push(next_tile.clone());
+        // println!("{next_tile:?}");
+        let next_tile = &path.iter().last().unwrap().get_next_tile(&grid).unwrap();
+        path.push(*next_tile);
+        let next_tile = &path.iter().last().unwrap().get_next_tile(&grid).unwrap();
+        path.push(*next_tile);
+        let next_tile = &path.iter().last().unwrap().get_next_tile(&grid).unwrap();
+        path.push(*next_tile);
+        let next_tile = &path.iter().last().unwrap().get_next_tile(&grid).unwrap();
+        path.push(*next_tile);
+        let next_tile = &path.iter().last().unwrap().get_next_tile(&grid).unwrap();
+        path.push(*next_tile);
+        let next_tile = &path.iter().last().unwrap().get_next_tile(&grid).unwrap();
+        path.push(*next_tile);
+        for t in &path {
+            println!("{t:?}");
+        }
     }
     #[test]
     fn day10_test() {
