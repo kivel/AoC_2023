f02e18e3defb749ec9b996168071f70801e4f277
Niko Kivel <niko.kivel@psi.ch>
2023-12-14 00:14:20 -0600

    day 14 part 1, OK

--- src/bin/day14_1.rs
+++ src/bin/day14_1.rs
@@ -2,7 +2,57 @@
 mod advent_of_code;
 
 fn day14_1(data: &str) -> usize {
-    todo!()
+    let d = data
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    let dish = advent_of_code::transpose(d);
+
+    // for line in &dish {
+    //     println!("{:?}", roll_line(line));
+    // }
+
+    dish.iter()
+        .map(|line| {
+            let line = roll_line(line);
+            line.iter()
+                .enumerate()
+                .filter_map(|(i, &c)| if c == 'O' { Some(i + 1) } else { None })
+                .sum::<usize>()
+        })
+        .sum::<usize>()
+}
+
+fn roll_line(line: &Vec<char>) -> Vec<char> {
+    let mut rolled_line: Vec<char> = Vec::new();
+    let mut buffer: Vec<char> = Vec::new();
+    for c in line {
+        match c {
+            'O' => {
+                if buffer.is_empty() {
+                    rolled_line.push(*c)
+                } else {
+                    buffer.insert(0, *c);
+                    // // rolled_line.push(*c);
+                    // rolled_line.extend(&buffer);
+                    // buffer = Vec::new();
+                }
+            }
+            '.' => buffer.push(*c),
+            '#' => {
+                buffer.push(*c);
+                rolled_line.extend(&buffer);
+                buffer = Vec::new();
+            }
+            _ => unreachable!(),
+        }
+    }
+    if !buffer.is_empty() {
+        rolled_line.extend(&buffer);
+    }
+    rolled_line.reverse();
+    rolled_line
 }
 
 fn main() {
@@ -13,7 +63,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{advent_of_code, day14_1};
+    use crate::{advent_of_code, day14_1, roll_line};
 
     #[test]
     fn load_calc_test() {
@@ -50,10 +100,40 @@ O..#.OO...
     }
 
     #[test]
+    fn roll_test() {
+        let input = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
+        let d = input
+            .lines()
+            .map(|line| line.chars().collect::<Vec<char>>())
+            // .rev()
+            .collect::<Vec<Vec<char>>>();
+        let dish = advent_of_code::transpose(d);
+        for line in &dish {
+            println!("{:?}", line);
+        }
+        //single line
+        let mut line = dish[0].clone();
+        let mut rolled_line = roll_line(&line);
+
+        println!("src:    {line:?}");
+        println!("rolled: {rolled_line:?}");
+    }
+
+    #[test]
     fn day14_1_test() {
         let d = include_str!("../../input/day14_test.txt");
         let result = day14_1(d);
-        assert_eq!(result, 405);
+        println!("{result:?}");
+        assert_eq!(result, 136)
     }
 
     #[test]
@@ -61,6 +141,6 @@ O..#.OO...
         let d = include_str!("../../input/day14.txt");
         let result = day14_1(d);
         println!("{}", result);
-        assert_eq!(result, 35691);
+        assert_eq!(result, 113456);
     }
 }
