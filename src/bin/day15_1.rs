8c953bf7bcec8d5db7604527427b6022ee3a5e04
Niko Kivel <niko.kivel@psi.ch>
2023-12-14 23:41:49 -0600

    typo

--- src/bin/day15_1.rs
+++ src/bin/day15_1.rs
@@ -1,13 +1,13 @@
 #[path = "../advent_of_code/mod.rs"]
 mod advent_of_code;
 
-fn day14_1(data: &str) -> usize {
+fn day15_1(data: &str) -> usize {
     data.split(',').map(|s| hasher(s)).sum::<usize>()
 }
 
 fn main() {
     let d = include_str!("../../input/day15.txt");
-    let result = day14_1(d);
+    let result = day15_1(d);
     println!("{}", result);
 }
 
@@ -22,7 +22,7 @@ fn hasher(input: &str) -> usize {
 }
 #[cfg(test)]
 mod tests {
-    use crate::{day14_1, hasher};
+    use crate::{day15_1, hasher};
 
     #[test]
     fn test_hasher() {
@@ -31,17 +31,17 @@ mod tests {
         assert_eq!(hash, 52)
     }
     #[test]
-    fn day14_1_test() {
+    fn day15_1_test() {
         let d = include_str!("../../input/day15_test.txt");
-        let result = day14_1(d);
+        let result = day15_1(d);
         println!("{result:?}");
         assert_eq!(result, 1320)
     }
 
     #[test]
-    fn day14_1_final() {
+    fn day15_1_final() {
         let d = include_str!("../../input/day15.txt");
-        let result = day14_1(d);
+        let result = day15_1(d);
         println!("{}", result);
         assert_eq!(result, 506891);
     }
